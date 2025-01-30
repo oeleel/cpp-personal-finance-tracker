a41f9ee78c058b72c9c238b810b5afc5db1c6291
Leo Lee <leosungwonlee@gmail.com>
2025-01-30 16:16:42 -0500

    controller files creation

---
+++ controllers/TestCtrl.cc
@@ -0,0 +1,6 @@
+#include "TestCtrl.h"
+
+void TestCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
